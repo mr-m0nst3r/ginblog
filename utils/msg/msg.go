96f21f7e44c0d2e18568e00425b9bc3d5216716d
m0nst3r <songxinlei@gmail.com>
2021-01-04 13:52:33 +0800

    class 3

---
+++ utils/msg/msg.go
@@ -0,0 +1,37 @@
+package msg
+
+const (
+	SUCCESS = 200
+	ERROR = 500
+
+	// code = 1000... user msg
+
+	ErrUsernameUsed   = 1001
+	ErrPasswordWrong  = 1002
+	ErrUserNotExist   = 1003
+	ErrTokenNotExist  = 1004
+	ErrTokenExpired   = 1005
+	ErrTokenWrong     = 1006
+	ErrTokenTypeWrong = 1007
+
+	// code = 2000... article msg
+
+
+	// code =  3000... category msg
+)
+
+var Codemsg = map[int]string{
+	SUCCESS:           "OK",
+	ERROR:             "FAIL",
+	ErrUsernameUsed:   "用户名已存在",
+	ErrPasswordWrong:  "密码错误",
+	ErrUserNotExist:   "用户不存在",
+	ErrTokenNotExist:  "TOKEN不存在",
+	ErrTokenExpired:   "TOKEN已过期",
+	ErrTokenWrong:     "TOKEN不正确",
+	ErrTokenTypeWrong: "TOKEN格式错误",
+}
+
+func GetMsg(code int) string  {
+	return Codemsg[code]
+}
