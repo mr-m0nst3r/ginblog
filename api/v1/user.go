e74ef600f69a80449fd3ab49811c529d99ca935e
m0nst3r <songxinlei@gmail.com>
2021-01-04 16:42:47 +0800

    class 4

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,15 +1,33 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/model"
+	"ginblog/utils/msg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
-// 查询用户是否存在
-func UserExist(c *gin.Context) {
-
-}
+var code int
 
 // 添加用户
 func AddUser(c *gin.Context) {
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == msg.SUCCESS {
+		model.CreateUser(&data)
+	}
 
+	if code == msg.ErrUsernameUsed {
+		code = msg.ErrUsernameUsed
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": code,
+		"data": data,
+		"msg": msg.GetMsg(code),
+	})
 }
 
 // 查询单个用户
@@ -17,7 +35,25 @@ func AddUser(c *gin.Context) {
 
 // 查询用户列表
 func GetUsers(c *gin.Context) {
+	pageSize,_ := strconv.Atoi(c.Query("pageSize"))
+	pageNum,_ := strconv.Atoi(c.Query("pageNum"))
 
+	if pageSize == 0 {
+		pageSize = -1
+	}
+
+	if pageNum == 0 {
+		pageNum = -1
+	}
+
+	data := model.GetUsers(pageSize, pageNum)
+	code = msg.SUCCESS
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":code,
+		"data":data,
+		"msg":msg.GetMsg(code),
+	})
 }
 
 // 编辑用户
--- model/User.go
+++ model/User.go
@@ -1,10 +1,43 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"ginblog/utils/msg"
+	"github.com/jinzhu/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string  `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role int `gorm:"type:int" json:"role"`
+}
+
+func CheckUser(name string) (code int)  {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+
+	if users.ID > 0 {
+		return msg.ErrUsernameUsed
+	}
+
+	return msg.SUCCESS
+}
+
+func CreateUser(data *User) int {
+	err = db.Create(&data).Error
+	if err != nil {
+		return msg.ERROR
+	}
+
+	return msg.SUCCESS
+}
+
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err = db.Limit(pageSize).Offset((pageNum-1)*pageSize).Find(&users).Error
+
+	if err != nil && err!=gorm.ErrRecordNotFound{
+		return nil
+	}
+	return users
 }
