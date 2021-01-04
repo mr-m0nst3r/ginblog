9c799e1c81abf91985a7b8b791033764f3bcaea3
m0nst3r <songxinlei@gmail.com>
2021-01-04 17:37:03 +0800

    class 5

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/smartystreets/goconvey v1.6.4 // indirect
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 	gopkg.in/ini.v1 v1.62.0
 )
--- model/User.go
+++ model/User.go
@@ -1,8 +1,11 @@
 package model
 
 import (
+	"encoding/base64"
 	"ginblog/utils/msg"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/scrypt"
+	"log"
 )
 
 type User struct {
@@ -24,6 +27,7 @@ func CheckUser(name string) (code int)  {
 }
 
 func CreateUser(data *User) int {
+	data.Password = EncryptPw(data.Password)
 	err = db.Create(&data).Error
 	if err != nil {
 		return msg.ERROR
@@ -40,4 +44,18 @@ func GetUsers(pageSize int, pageNum int) []User {
 		return nil
 	}
 	return users
+}
+
+func EncryptPw(password string) string {
+	const KeyLen = 10
+	salt := make([]byte, 8)
+	salt = []byte{12,32,4,6,66,22,222,11}
+
+	HashPw, err := scrypt.Key([]byte(password), salt, 16384,8,1,KeyLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	FinalPw := base64.StdEncoding.EncodeToString(HashPw)
+	return FinalPw
 }
